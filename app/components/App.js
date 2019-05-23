42442c7235b0b32721b7fbd8b46f2263edc2c86d
Saranya Easwaran <Saranya.amr@gmail.com>
2019-05-23 10:38:28 +0530

    Changed the welcome message

--- app/components/App.js
+++ app/components/App.js
@@ -10,123 +10,126 @@ import NewRoomForm from './NewRoomForm';
 
 class App extends React.Component {
 
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            messageList : [],
-            joinableRooms :[],
-            joinedRooms :[],
-            currentRoomId:'',
-            inputDisableValue:true  
+            messageList: [],
+            joinableRooms: [],
+            joinedRooms: [],
+            currentRoomId: '',
+            inputDisableValue: true
 
         }
-        
-        this.sendMessageFromUser=this.sendMessageFromUser.bind(this)
+
+        this.sendMessageFromUser = this.sendMessageFromUser.bind(this)
         this.getRoomList = this.getRoomList.bind(this)
         this.subscribeToRoom = this.subscribeToRoom.bind(this)
         this.newRoomFromUser = this.newRoomFromUser.bind(this)
-        
+
     }
-    
+
     componentDidMount() {
-        
-        const userId1 = prompt(("Please enter your name", "Harry Potter"))
+
+        const userId1 = prompt(("Please enter your name", "e.g. anyone"))
         console.log(userId1)
 
         const chatManager = new Chatkit.ChatManager({
             instanceLocator,
             // userId: 'saranya',
-            userId : userId1,
+            userId: userId1,
             tokenProvider: new Chatkit.TokenProvider({
                 url: tokenUrl
             })
         })
-        
+
         chatManager.connect()
-        .then(currentUser => {
-            this.currentUser = currentUser
-            
-            //To get the room list
-            this.getRoomList()
-            //To get the chat message in a specific room
-            // this.subscribeToRoom()
-        })    
-              
-    }
+            .then(currentUser => {
+                this.currentUser = currentUser
 
-    getRoomList(){
-        this.currentUser.getJoinableRooms()
-        .then(joinableRooms=>{
-            this.setState({
-                joinableRooms : joinableRooms,
-                joinedRooms : this.currentUser.rooms
+                //To get the room list
+                this.getRoomList()
+                    //To get the chat message in a specific room
+                    // this.subscribeToRoom()
             })
-        })
+
     }
 
-    subscribeToRoom(roomId){
+    getRoomList() {
+        this.currentUser.getJoinableRooms()
+            .then(joinableRooms => {
+                this.setState({
+                    joinableRooms: joinableRooms,
+                    joinedRooms: this.currentUser.rooms
+                })
+            })
+    }
+
+    subscribeToRoom(roomId) {
         const localMessageData = []
-        this.state.messageList=[]
+        this.state.messageList = []
         this.currentUser.subscribeToRoom({
-            roomId: roomId,
-            hooks: {
-                onMessage: message => {
-                    localMessageData.push(
-                        {"senderId" : message.senderId,
-                        "text" : message.text
-                        }   
-                    )
+                roomId: roomId,
+                hooks: {
+                    onMessage: message => {
+                        localMessageData.push({
+                            "senderId": message.senderId,
+                            "text": message.text
+                        })
 
-                            this.setState({
-                            messageList : localMessageData
-                        }) 
-                    
+                        this.setState({
+                            messageList: localMessageData
+                        })
+
+                    }
                 }
-            }
-            
+
+            })
+            //To set the current room id irrespective of the room has messages or not, and to set the chat input text field disable value. input field wil be enabled only if the user has joined a room
+            .then(this.setState({
+                currentRoomId: roomId,
+                inputDisableValue: false
+            }))
+            .then(room => this.getRoomList())
+    }
+
+    sendMessageFromUser(message) {
+
+        this.currentUser.sendMessage({
+            text: message,
+            roomId: this.state.currentRoomId
         })
-        //To set the current room id irrespective of the room has messages or not, and to set the chat input text field disable value. input field wil be enabled only if the user has joined a room
-        .then(this.setState({
-            currentRoomId : roomId,
-            inputDisableValue:false
-        }) )
-        .then(room=>this.getRoomList())
-    }
-
-    sendMessageFromUser(message){
-
-        this.currentUser.sendMessage(
-            {
-                text:message,
-                roomId : this.state.currentRoomId
-            }
-        )
 
     }
 
-    newRoomFromUser(roomName){
+    newRoomFromUser(roomName) {
         this.currentUser.createRoom({
-            name:roomName
-        })
-        .then(room=>this.subscribeToRoom(room.id))
+                name: roomName
+            })
+            .then(room => this.subscribeToRoom(room.id))
     }
 
-	render(){
-		return(
-			<div className="app">
-				<RoomList allRooms={[...this.state.joinableRooms,...this.state.joinedRooms]} 
-                subscribeToRoom = {this.subscribeToRoom} 
-                currentRoomId = {this.state.currentRoomId}
-                /> 
-				<MessageList messageList = {this.state.messageList} currentRoomId = {this.state.currentRoomId}/>
-				{/* <NewRoomForm /> */}
-                <NewRoomForm newRoom = {this.newRoomFromUser}/>
-                <SendMessageForm sendMessage={this.sendMessageFromUser} inputDisableValue={this.state.inputDisableValue}/>
-			</div>
-            
-            
-		)
-	}
+    render() {
+        return ( <
+            div className = "app" >
+            <
+            RoomList allRooms = {
+                [...this.state.joinableRooms, ...this.state.joinedRooms] }
+            subscribeToRoom = { this.subscribeToRoom }
+            currentRoomId = { this.state.currentRoomId }
+            />  <
+            MessageList messageList = { this.state.messageList }
+            currentRoomId = { this.state.currentRoomId }
+            /> { /* <NewRoomForm /> */ } <
+            NewRoomForm newRoom = { this.newRoomFromUser }
+            /> <
+            SendMessageForm sendMessage = { this.sendMessageFromUser }
+            inputDisableValue = { this.state.inputDisableValue }
+            /> <
+            /div>
+
+
+        )
+    }
 }
 
 export default App
